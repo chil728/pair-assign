82dbd855cda61eaa9d58fed62a1f56e869d1a87c
Tom <chanhinieong@gmail.com>
2025-04-04 17:11:21 +0800

    home page updated

--- CISC3003-PairAssgn-2025APR06/public/js/Bussiness/cisc3003-PairAssgn.js
+++ CISC3003-PairAssgn-2025APR06/public/js/Bussiness/cisc3003-PairAssgn.js
@@ -1,7 +1,17 @@
-var typingEffect = new Typed(".typedText", {
-    strings: ["Dev.", "Dev."],
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 2000,
-});
+document.addEventListener("DOMContentLoaded", () => {
+    var typingEffect = new Typed(".typedText", {
+        strings: ["Dev.", "Dev."],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 2000,
+    });
+
+    const toggleMenu = document.querySelector(".toggle-menu");
+    const navLinks = document.querySelector(".nav-links");
+
+    toggleMenu.addEventListener("click", () => {
+        toggleMenu.classList.toggle("active");
+        navLinks.classList.toggle("show");
+    })
+})
